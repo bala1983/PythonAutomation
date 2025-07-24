f46a217df03556b6d7325ffddc155b0b7e07c490
Oleksandr Bolokan <bala1983@ukr.net>
2025-07-24 18:16:41 +0300

    Виконані зміни

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -3,11 +3,9 @@ import unittest
 import sys
 import pathlib
 
-from PythonAutomation.lesson_09.homework_09_def import car_data_negative
-
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent))
-from homework_09_def import filter_cars, car_data, car_data_negative
-
+from PythonAutomation.lesson_09.homework_09_def import (total_area, filter_cars, car_data, car_data_negative,
+                                                        sum_all_numbers)
 
 class CarTest(unittest.TestCase):
     def test_filter_cars_positive(self):
@@ -32,6 +30,66 @@ class CarTest(unittest.TestCase):
         expected_error = "Search criteria is invalid"
         self.assertEqual(expected_error, actual_error)
 
+
+class AreaTest(unittest.TestCase):
+    def test_total_area_positive(self):
+        actual_result = total_area(436402,37800)
+        expected_result = f"Площа Чорного та Азовського морів дорівнює: ,474202, км²"
+
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_total_area_negative(self):
+        actual_result = total_area(436402,37800)
+        expected_result = f"Площа Чорного та Азовського морів дорівнює: ,500000, км²"
+
+        self.assertNotEqual(expected_result, actual_result)
+
+
+class SumTest(unittest.TestCase):
+    def test_sum_all_positive(self):
+        input_data = ["1,2,3,4", "1,2,3,4,50"]
+        actual_result = sum_all_numbers(input_data)
+        expected_result = [10, 60]
+        print(f"Input data: {input_data}")
+        print(f"Actual result: {actual_result}", f"Expected result: {expected_result}")
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_sum_all_negative(self):
+        input_data = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+        actual_result = sum_all_numbers(input_data)
+        expected_result = [10, 60, "Не можу це зробити!"]
+        print(f"Input data: {input_data}")
+        print(f"Actual result: {actual_result}", f"Expected result: {expected_result}")
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_sum_all_negative_with_two_arg(self):
+        input_data = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3", "5,5"]
+        actual_result = sum_all_numbers(input_data)
+        expected_result = [10, 60, "Не можу це зробити!", 10]
+        print(f"Input data: {input_data}")
+        print(f"Actual result: {actual_result}", f"Expected result: {expected_result}")
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_sum_all_negative_invalid_data(self):
+        input_data = ["1,2,3,4", 2, "1,2,3,4,50"]
+        actual_result = sum_all_numbers(input_data)
+        expected_result = [10, 60]
+        print(f"Input data: {input_data}")
+        print(f"Actual result: {actual_result}", f"Expected result: {expected_result}")
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_sum_all_negative_positive_single_number(self):
+        input_data = ["50"]
+        actual_result = sum_all_numbers(input_data)
+        expected_result = [50]
+        print(f"Input data: {input_data}")
+        print(f"Actual result: {actual_result}", f"Expected result: {expected_result}")
+        self.assertEqual(expected_result, actual_result)
 
 if __name__ == "__main__":
     unittest.main()
--- lesson_09/homework_09_def.py
+++ lesson_09/homework_09_def.py
@@ -1,28 +1,6 @@
 def total_area(area1, area2):
     area = area1 + area2
     return f"Площа Чорного та Азовського морів дорівнює: ,{area}, км²"
-# print(total_area(area1=436402, area2=37800))
-
-
-def warehouses(total_warehouses, first_warehouse_and_second_warehouse, second_warehouse_and_third_warehouse):
-    second = first_warehouse_and_second_warehouse + second_warehouse_and_third_warehouse - total_warehouses
-    first = first_warehouse_and_second_warehouse - second
-    third = second_warehouse_and_third_warehouse - second
-    return f"Перший склад: {first}, Другий склад: {second}, Третій склад: {third}"
-# print(warehouses(375291, 250449,222950))
-
-
-def pc_price(mouths, years, monthly_payment):
-    mouths = mouths * years
-    price = int(mouths * monthly_payment)
-    return f"Вартість комп'ютера: {price} грн."
-# print(pc_price(12, 1.5,1179))
-
-
-def number_of_pages_in_album(photos, per_page):
-    all_pages = (photos + per_page -1) // per_page
-    return f"Потрібно сторінок: {all_pages}"
-# print(number_of_pages_in_album(232,8))
 
 
 def filter_cars(car_dict: dict, search_criteria: tuple) -> dict:
@@ -126,3 +104,21 @@ car_data_negative = {
   'Ford F-Series': ('gray', 2021, 3.5, 'pickup', 50000),
   'Nissan Titan': ('silver', 2018, 5.6, 'pickup', 35000)
 }
+
+list1 = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+
+def sum_all_numbers(numbers:list[str])->list:
+    result_list = []
+    for number in numbers:
+        try:
+            split_number = [int(x) for x in number.split(",")]
+            result_list.append(sum(split_number))
+            # print(sum(split_number))
+        except ValueError as e:
+            print(f"Не можу це зробити!: {e}")
+            result_list.append("Не можу це зробити!")
+        except AttributeError as e:
+            print(f"Invalid input type: {e}")
+    return result_list
+
+print(sum_all_numbers(list1))
