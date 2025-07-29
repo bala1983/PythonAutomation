c5ce95aa53ab5f6a12055fa14cba89cb25f1d345
Oleksandr Bolokan <bala1983@ukr.net>
2025-07-29 18:08:44 +0300

    Виконані зміни

--- dop_lesson/test_homework_10.py
+++ dop_lesson/test_homework_10.py
@@ -2,21 +2,34 @@ import pytest
 
 from PythonAutomation.dop_lesson.def_change_letters import change_letter
 
+# @pytest.mark.positive
+# class TestChangeLettersPositive:
+#     def test_one_word(self):
+#         input_data = "hello"
+#         actual_result = change_letter(input_data)
+#         expected_result = "HeLlO"
+#         assert actual_result == expected_result
+#
+#     def test_big_sentence(self):
+#         input_data = "Would you tell me, please, which way I ought to go from here?"
+#         actual_result = change_letter(input_data)
+#         expected_result = 'WoUlD yOu tElL mE, pLeAsE, wHiCh wAy i oUgHt tO gO fRoM hErE?'
+#         assert actual_result == expected_result
 
+
+@pytest.mark.positive
 class TestChangeLettersPositive:
-    def test_one_word(self):
-        input_data = "hello"
-        actual_result = change_letter(input_data)
-        expected_result = "HeLlO"
-        assert actual_result == expected_result
 
-    def test_big_sentence(self):
-        input_data = "Would you tell me, please, which way I ought to go from here?"
-        actual_result = change_letter(input_data)
-        expected_result = 'WoUlD yOu tElL mE, pLeAsE, wHiCh wAy i oUgHt tO gO fRoM hErE?'
-        assert actual_result == expected_result
+    @pytest.mark.parametrize('function_input, expected_result',[
+        ("hello", "HeLlO"),
+        ("Would you tell me, please, which way I ought to go from here?",
+         'WoUlD yOu tElL mE, pLeAsE, wHiCh wAy i oUgHt tO gO fRoM hErE?'),
+    ])
+    def test_one_word_big_sentence(self, function_input, expected_result):
+        assert change_letter(function_input) == expected_result
 
 
+@pytest.mark.negative
 class TestChangeLettersNegative:
     def test_empty_sentence(self):
         input_data = ''
@@ -33,4 +46,3 @@ class TestChangeLettersNegative:
         actual_result = change_letter(input_data)
         expected_result = '-10292682681'
         assert actual_result == expected_result
-
