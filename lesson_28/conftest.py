95930dbfa51c0d69bcd156a21bc2682c5cb00adf
Oleksandr Bolokan <bala1983@ukr.net>
2025-10-27 21:32:48 +0200

    "Виконані зміни"

--- lesson_28/conftest.py
+++ lesson_28/conftest.py
@@ -1,18 +1,22 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from PythonAutomation.lesson_28.main_page import MainPage
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless=new")  # обов’язково для Jenkins
-    options.add_argument("--no-sandbox")  # потрібне у контейнерах
-    options.add_argument("--disable-dev-shm-usage")  # запобігає падінням у Linux
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(options=options)
+    service = Service()
+
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
 
