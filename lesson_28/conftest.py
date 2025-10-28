f5bbc6fefc46a0ef6b1e6466929e38d7ea60d34e
Oleksandr Bolokan <bala1983@ukr.net>
2025-10-28 19:58:51 +0200

    "Виконані зміни"

--- lesson_28/conftest.py
+++ lesson_28/conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from PythonAutomation.lesson_28.main_page import MainPage
-from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
